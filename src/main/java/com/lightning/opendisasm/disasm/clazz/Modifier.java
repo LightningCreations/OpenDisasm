b7c02043e23b7271bdb76369afd3fad8c92b27a8
Connor Horman <chorman64@gmail.com>
2019-06-15 11:49:37 -0400

    Start Modifiers

---
+++ src/main/java/com/lightning/opendisasm/disasm/clazz/Modifier.java
@@ -0,0 +1,27 @@
+package com.lightning.opendisasm.disasm.clazz;
+
+public class Modifier {
+	public enum ModifierDomain{
+		Class, Field,
+		Method, Local,
+		InnerClass;
+	}
+	
+	private int value;
+	private ModifierDomain domain;
+	
+	public static interface Class{
+		Modifier ACC_PUBLIC = new Modifier(0x0001,ModifierDomain.Class);
+		Modifier ACC_FINAL = new Modifier(0x0010,ModifierDomain.Class);
+		
+	}
+	
+	private Modifier(int mask,ModifierDomain domain) {
+		this.value = mask;
+		this.domain = domain;
+	}
+	
+	public int getMask() {
+		return value;
+	}
+}
