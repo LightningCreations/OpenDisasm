0ff24a8ee6517901da98116aec258a58e67bfa02
Ray Redondo <rdrpenguin04@gmail.com>
2022-01-09 19:16:59 -0600

    interface

--- od_core/src/lib.rs
+++ od_core/src/lib.rs
@@ -1,23 +1,31 @@
+#![feature(once_cell)]
+
+#[link(name = "od_interface")] extern "C"{}
+
 use std::fs::File;
+use std::lazy::SyncOnceCell;
+use xlang_abi::io::ReadAdapter;
+use xlang_abi::traits::DynMut;
 
 #[repr(C)]
 pub struct Tree;
 
 pub mod abi_safe {
     pub use super::Tree;
-use xlang_abi::traits::DynPtrSafe;
+    use xlang_abi::traits::DynMut;
 
     pub enum Error {
         InvalidFile(xlang_abi::string::String),
         Io(xlang_abi::io::Error),
         MoreInfoNeeded(xlang_abi::string::String), // Where the string represents a regex of a file that could provide the additional information
+        Unrecognized,
     }
 
     pub type Result<T> = xlang_abi::result::Result<T, Error>;
 
     pub trait Disassembler {
-        fn can_read(file: dyn DynPtrSafe<dyn xlang_abi::io::Read>) -> Result<bool>;
-        fn disassemble(file: dyn DynPtrSafe<dyn xlang_abi::io::Read>) -> Result<Tree>;
+        fn can_read<'a>(&self, file: DynMut<dyn xlang_abi::io::Read + 'a>) -> Result<bool>;
+        fn disassemble<'a>(&self, file: DynMut<dyn xlang_abi::io::Read + 'a>) -> Result<Tree>;
     }
 }
 
@@ -25,6 +33,7 @@ pub enum Error {
     InvalidFile(String),
     Io(std::io::Error),
     MoreInfoNeeded(String),
+    Unrecognized,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -35,10 +44,25 @@ impl From<abi_safe::Error> for Error {
             abi_safe::Error::InvalidFile(x) => Self::InvalidFile(x.to_string()),
             abi_safe::Error::Io(x) => Self::Io(x.into()),
             abi_safe::Error::MoreInfoNeeded(x) => Self::MoreInfoNeeded(x.to_string()),
+            abi_safe::Error::Unrecognized => Self::Unrecognized,
         }
     }
 }
 
-pub fn load_file(_: File) -> crate::Result<Tree> {
-    Ok(Tree)
+static DISASSEMBLERS: SyncOnceCell<Vec<Box<dyn abi_safe::Disassembler + Send + Sync>>> = SyncOnceCell::new();
+
+pub fn init_list() -> Vec<Box<dyn abi_safe::Disassembler + Send + Sync>> {
+    let result = Vec::new();
+    // TODO
+    result
+}
+
+pub fn load_file(file: File) -> Result<Tree> {
+    let disassemblers = DISASSEMBLERS.get_or_init(init_list);
+    for disassembler in disassemblers {
+        if Result::from(disassembler.can_read(DynMut::unsize_mut(&mut ReadAdapter::new(&file))).map_err(Into::into))? {
+            return Result::from(disassembler.disassemble(DynMut::unsize_mut(&mut ReadAdapter::new(file))).map_err(Into::into));
+        }
+    }
+    Err(Error::Unrecognized)
 }
--- od_interface/src/lib.rs
+++ od_interface/src/lib.rs
@@ -1,8 +1,54 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use core::alloc::Layout;
+
+#[no_mangle]
+pub extern "C" fn xlang_allocate(size: usize) -> *mut core::ffi::c_void {
+    if size == 0 {
+        return 32usize as *mut core::ffi::c_void;
     }
+    unsafe {
+        xlang_allocate_aligned(
+            size,
+            if size > 32 {
+                32
+            } else {
+                size.next_power_of_two()
+            },
+        )
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn xlang_allocate_aligned(
+    size: usize,
+    align: usize,
+) -> *mut core::ffi::c_void {
+    if size == 0 {
+        return align as *mut core::ffi::c_void;
+    }
+    let size = size + (align - size % align) % align;
+    let layout = Layout::from_size_align_unchecked(size, align);
+    std::alloc::alloc(layout).cast::<_>()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn xlang_deallocate_aligned(
+    ptr: *mut core::ffi::c_void,
+    size: usize,
+    align: usize,
+) {
+    if size == 0 || ptr.is_null() {
+        return;
+    }
+    let size = size + (align - size % align) % align;
+    let layout = Layout::from_size_align_unchecked(size, align);
+    std::alloc::dealloc(ptr.cast::<_>(), layout);
+}
+
+#[no_mangle]
+pub extern "C" fn xlang_on_allocation_failure(size: usize, align: usize) -> ! {
+    eprintln!(
+        "Failed to allocate with size: {}, and alignment: {}",
+        size, align
+    );
+    std::process::abort()
 }
